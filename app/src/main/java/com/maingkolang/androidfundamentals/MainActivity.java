27fd96dddee14a3528367b2fb8441420df082988
nirwanto.maingkolang <mr.maingkolang@gmail.com>
2017-06-05 17:24:12 +0700

    Commit: MainActivity.java build.gradle (project) build.gradle (app)

--- app/src/main/java/com/maingkolang/androidfundamentals/MainActivity.java
+++ app/src/main/java/com/maingkolang/androidfundamentals/MainActivity.java
@@ -1,13 +1,16 @@
 package com.maingkolang.androidfundamentals;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.multidex.MultiDex;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.a_main);
+        setContentView(R.layout.a_layout);
+
+        MultiDex.install(this);
     }
 }
